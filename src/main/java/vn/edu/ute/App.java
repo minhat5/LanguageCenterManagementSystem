7b8a52c5df5bfbbc80cff1727ca422dc3275ab4d
TinMH <tinhongmai1012@gmail.com>
2026-03-09 14:16:19 +0700

    Complete ui for promotion module

--- src/main/java/vn/edu/ute/App.java
+++ src/main/java/vn/edu/ute/App.java
@@ -1,13 +1,12 @@
 package vn.edu.ute;
 
 import vn.edu.ute.db.TransactionManager;
-import vn.edu.ute.repo.impl.ClasRepoImpl;
-import vn.edu.ute.repo.impl.EnrollmentRepoImpl;
-import vn.edu.ute.repo.impl.PlacementTestRepoImpl;
-import vn.edu.ute.repo.impl.StudentRepoImpl;
+import vn.edu.ute.repo.impl.*;
 import vn.edu.ute.service.impl.EnrollmentServiceImpl;
+import vn.edu.ute.service.impl.PromotionServiceImpl;
 import vn.edu.ute.ui.EnrollmentPanel;
 import vn.edu.ute.ui.PlacementTestPanel;
+import vn.edu.ute.ui.promotion.PromotionPanel;
 
 import javax.swing.*;
 
@@ -23,15 +22,18 @@ public class    App {
                 var enrollmentRepo = new EnrollmentRepoImpl();
                 var clasRepo = new ClasRepoImpl();
                 var studentRepo = new StudentRepoImpl();
+                var promotionRepo = new PromotionRepoImpl();
 
                 // 3. Khởi tạo Service (Bơm TxManager và Repositories vào)
                 var enrollmentService = new EnrollmentServiceImpl(
                         txManager, testRepo, enrollmentRepo, clasRepo, studentRepo
                 );
+                var promotionService  = new PromotionServiceImpl(txManager, promotionRepo);
 
                 // 4. Khởi tạo UI và nhúng Panel
                 PlacementTestPanel testPanel = new PlacementTestPanel(enrollmentService);
                 EnrollmentPanel enrollPanel = new EnrollmentPanel(enrollmentService);
+                PromotionPanel promotionPanel = new PromotionPanel(promotionService);
 
                 JFrame frame = new JFrame("Test Module Tuyển Sinh - Mai Hồng Tín");
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -42,6 +44,8 @@ public class    App {
                 JTabbedPane tabbedPane = new JTabbedPane();
                 tabbedPane.addTab("1. Quản Lý Đánh Giá Năng Lực", testPanel);
                 tabbedPane.addTab("2. Quản Lý Ghi Danh", enrollPanel);
+                tabbedPane.addTab("2. Quản Lý  Mã Giảm Giá", promotionPanel);
+
 
                 frame.add(tabbedPane);
                 frame.setVisible(true);
